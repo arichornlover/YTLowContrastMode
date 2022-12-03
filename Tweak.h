85e7c127c0bc1ce89b795e862af7dc06d2960a0c
Aric <78001398+arichorn@users.noreply.github.com>
2022-12-03 15:45:13 -0600

    New Headers (v1.1.0)

--- Tweak.h
+++ Tweak.h
@@ -1,9 +1,14 @@
 #import <UIKit/UIKit.h>
+#import "../YouTubeHeader/YTSettingsCell.h"
+#import "../YouTubeHeader/YTSettingsSectionItem.h"
+#import "../YouTubeHeader/YTSettingsSectionItemManager.h"
+#import "../YouTubeHeader/YTSettingsViewController.h"
+#import "../YouTubeHeader/YTPlayerViewController.h"
 
-@interface YTColorPalette : NSObject
+@interface YTColorPalette : NSObject // YTColorPalette is here to Support older YouTube Versions.
 @property(readonly, nonatomic) long long pageStyle;
 @end
 
-@interface YTCommonColorPalette : NSObject
+@interface YTCommonColorPalette : NSObject // YTCommonColorPalette is here to Support modern YouTube Versions.
 @property(readonly, nonatomic) long long pageStyle;
 @end
