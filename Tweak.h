4ae70f9528a2fceb8db3b6498400d5950d9b804e
Aric <78001398+arichorn@users.noreply.github.com>
2022-12-03 19:37:52 -0600

    Disabled some Headers

--- Tweak.h
+++ Tweak.h
@@ -1,9 +1,9 @@
 #import <UIKit/UIKit.h>
-#import "../YouTubeHeader/YTSettingsCell.h"
-#import "../YouTubeHeader/YTSettingsSectionItem.h"
-#import "../YouTubeHeader/YTSettingsSectionItemManager.h"
-#import "../YouTubeHeader/YTSettingsViewController.h"
-#import "../YouTubeHeader/YTPlayerViewController.h"
+// #import "../YouTubeHeader/YTSettingsCell.h"
+// #import "../YouTubeHeader/YTSettingsSectionItem.h"
+// #import "../YouTubeHeader/YTSettingsSectionItemManager.h"
+// #import "../YouTubeHeader/YTSettingsViewController.h"
+// #import "../YouTubeHeader/YTPlayerViewController.h"
 
 @interface YTColorPalette : NSObject // YTColorPalette is here to Support older YouTube Versions.
 @property(readonly, nonatomic) long long pageStyle;
