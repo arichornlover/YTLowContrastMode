1a1faf8f002935d0c3d4dfa020fb03407d5e745e
arichorn <78001398+arichorn@users.noreply.github.com>
2023-07-10 18:42:39 -0500

    Update Tweak.h

--- Tweak.h
+++ Tweak.h
@@ -1,15 +1,31 @@
 #import <UIKit/UIKit.h>
 #import <rootless.h>
-// #import "../YouTubeHeader/YTSettingsCell.h"
-// #import "../YouTubeHeader/YTSettingsSectionItem.h"
-// #import "../YouTubeHeader/YTSettingsSectionItemManager.h"
-// #import "../YouTubeHeader/YTSettingsViewController.h"
-// #import "../YouTubeHeader/YTPlayerViewController.h"
 
-@interface YTColorPalette : NSObject // YTColorPalette is here to Support older YouTube Versions.
+@interface YTSettingsCell : YTCollectionViewCell
+@end
+
+@interface YTSettingsSectionItem : NSObject
+@property BOOL hasSwitch;
+@property BOOL switchVisible;
+@property BOOL on;
+@property BOOL (^switchBlock)(YTSettingsCell *, BOOL);
+@property int settingItemId;
+- (instancetype)initWithTitle:(NSString *)title titleDescription:(NSString *)titleDescription;
+@end
+
+@interface _ASCollectionViewCell : UICollectionViewCell
+- (id)node;
+@end
+
+@interface _ASDisplayView : UIView
+- (UILabel *)findLabelInSubviews:(NSArray *)subviews;
+- (void)customizeLabel:(UILabel *)label;
+@end
+
+@interface YTColorPalette : NSObject // YTColorPalette [YouTube v16.05.7-v16.46.5]
 @property(readonly, nonatomic) long long pageStyle;
 @end
 
-@interface YTCommonColorPalette : NSObject // YTCommonColorPalette is here to Support modern YouTube Versions.
+@interface YTCommonColorPalette : NSObject // YTCommonColorPalette [17.01.4-latest]
 @property(readonly, nonatomic) long long pageStyle;
 @end
